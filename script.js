c2d16b08b81e44a08f5ec44a022d90417fee8402
Jacob Anderson <jacobdanderson@gmail.com>
2021-02-26 23:33:26 -0700

    Final touches, including add API and formatting, ip detector shown in lower right corner

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-document.getElementById("infoSubmit").addEventListener("click", function(event) {
-    event.preventDefault();
-    const email = document.getElementById("emailInput").value;
-    if (email === "") return;
-    console.log(email);
-
-    let removed_credential = "";
-    const url = "http://example.invalid/api/check?removed_credential = 'REDACTED_APILAYER_ACCESS_KEY'&email=" + email + "&stmp=1&format=1";
-    fetch(url).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        let emailValid = "";
-        if(json.format_valid === true){
-            emailValid += "Valid email!" ;
-        } else {
-            emailValid += "Invalid email. Try again. ";
-        }
-        document.getElementById("valid").innerHTML = emailValid;
-        console.log(json.format_valid);
-    });
-
-});
 
 const updateQuote = async () => {
     const url = "https://quote-garden.herokuapp.com/api/v3/quotes?genre=success&limit=100";
@@ -37,6 +15,42 @@ updateQuote().then(data => {
     results += "<br>"
     results += "&emsp;&emsp;&emsp;-" + quote.quoteAuthor;
     document.getElementById("quotation").innerHTML = results;
+});
+
+const updateIp = async () => {
+    const url = "https://api64.ipify.org?format=json";
+    let response = await fetch(url);
+    let json = await response.json();
+    console.log(json);
+    return json;
+}
+
+updateIp().then(data => { document.getElementById("ip").innerHTML = data.ip; });
+
+document.getElementById("infoSubmit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const email = document.getElementById("emailInput").value;
+    if (email === "") return;
+    console.log(email);
+
+    let removed_credential = "";
+    const url = "http://example.invalid/api/check?removed_credential = 'REDACTED_APILAYER_ACCESS_KEY'&email=" + email + "&stmp=1&format=1";
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        let emailValid = "";
+        if(json.format_valid === true) {
+            document.getElementById("valid").style.color = "green";
+            emailValid += "Valid email!" ;
+        } else {
+            document.getElementById("valid").style.color = "red";
+            emailValid += "Invalid email. Try again.";
+        }
+        document.getElementById("valid").innerHTML = emailValid;
+        document.getElementById("valid").style.display = "block";
+        console.log(json.format_valid);
+    });
+
 });
 
 
