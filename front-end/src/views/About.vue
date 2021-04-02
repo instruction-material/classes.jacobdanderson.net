42825e8b10e6dc00ed3caf147aa1e891d088dce1
Jacob Anderson <jacobdanderson@gmail.com>
2021-04-02 12:14:53 -0600

    Setting up Creative Project 4

---
+++ front-end/src/views/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-------------
+  -   Section   -
+  -------------->
+
+  <section class="About text-center">
+    <h1>About Us</h1>
+    <h2>Mission:</h2>
+    <img
+      alt="Smiling creepy college student"
+      class="m-5"
+      src="https://tenneyschool.com/wp-content/uploads/2016/11/Confident-Student.jpg"
+      width="40%"
+    />
+    <p class="mt-3">
+      Here at Operation Opportunity, we have noticed a real need from the
+      students today for guidance when it comes to furthering their education.
+      It's important to be ready and confident for these big steps in life.
+      Taking good prep classes in high school, learning crucial study and note
+      taking skills, and not being afraid of the application and admittance
+      processes reduce stress and make students more successful All students
+      deserve a fair opportunity to higher education. That's what we are
+      committed to!
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "About",
+  components: {},
+  data() {},
+  computed: {},
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 15px;
+  margin: 0 10%;
+}
+</style>
