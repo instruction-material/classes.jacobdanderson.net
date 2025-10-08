1cba4737ddcf4891abe6bf4de4fdab2fe686a801
Jacob Anderson <26570578+Jacoba1100254352@users.noreply.github.com>
2025-10-08 13:37:23 -0400

    Serve favicon assets from public

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,28 +3,41 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-	title: "Classes",
-	meta: [
-		{
-			name: "Classes",
-			content: ""
-		},
-		{
-			name: "theme-color",
-			content: () => (isDark.value ? "#00aba9" : "#ffffff")
-		}
-	],
-	link: [
-		{
-			rel: "icon",
-			type: "image/svg+xml",
-			// This is identical for both dark and light mode for now
-			href: () =>
-				preferredDark.value
-					? "/Favicons/favicon.ico"
-					: "/Favicons/favicon.ico"
-		}
-	]
+        title: "Classes",
+        meta: [
+                {
+                        name: "Classes",
+                        content: ""
+                },
+                {
+                        name: "theme-color",
+                        content: () => (isDark.value ? "#00aba9" : "#ffffff")
+                }
+        ],
+        link: [
+                {
+                        rel: "icon",
+                        type: "image/x-icon",
+                        href: "/Favicons/favicon.ico"
+                },
+                {
+                        rel: "icon",
+                        type: "image/png",
+                        sizes: "32x32",
+                        href: "/Favicons/favicon-32x32.png"
+                },
+                {
+                        rel: "icon",
+                        type: "image/png",
+                        sizes: "16x16",
+                        href: "/Favicons/favicon-16x16.png"
+                },
+                {
+                        rel: "apple-touch-icon",
+                        sizes: "180x180",
+                        href: "/Favicons/apple-touch-icon.png"
+                }
+        ]
 });
 </script>
 
