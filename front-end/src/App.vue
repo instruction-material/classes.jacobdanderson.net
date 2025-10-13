96af523a5a7005e5db0d3475ed95ee971aa14dd3
Jacob Anderson <jacobdanderson@gmail.com>
2025-10-13 13:06:13 -0400

    Adjusting Favicon path and location

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
+// they will be rendered correctly in the HTML results with vite-ssg
 useHead({
 	title: "Classes",
 	meta: [
@@ -18,29 +18,29 @@ useHead({
 		{
 			rel: "icon",
 			type: "image/x-icon",
-			href: "/Favicons/favicon.ico"
+			href: "/assets/Favicons/favicon.ico"
 		},
 		{
 			rel: "icon",
 			type: "image/png",
 			sizes: "32x32",
-			href: "/Favicons/favicon-32x32.png"
+			href: "/assets/Favicons/favicon-32x32.png"
 		},
 		{
 			rel: "icon",
 			type: "image/png",
 			sizes: "16x16",
-			href: "/Favicons/favicon-16x16.png"
+			href: "/assets/Favicons/favicon-16x16.png"
 		},
 		{
 			rel: "apple-touch-icon",
 			sizes: "180x180",
-			href: "/Favicons/apple-touch-icon.png"
+			href: "/assets/Favicons/apple-touch-icon.png"
 		}
 		/*				{
 					rel: 'icon',
 					type: 'image/svg+xml',
-					href: () => preferredDark.value ? '/Favicons/favicon-dark.svg' : '/Favicons/favicon.svg',
+					href: () => preferredDark.value ? '/assets/Favicons/favicon-dark.svg': '/assets/Favicons/favicon.svg',
 				}, */
 	]
 });
