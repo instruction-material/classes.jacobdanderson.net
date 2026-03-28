b447ace58c8b91cf50b42b98da5063932fadb2a5
Jacob Anderson <jacobdanderson@gmail.com>
2026-03-28 09:50:29 -0400

    Polish homepage quote card

--- front-end/src/pages/index.vue
+++ front-end/src/pages/index.vue
@@ -62,11 +62,19 @@ onMounted(updateQuote);
 
 	<section class="Home text-center">
 		<h1>Operation Opportunity</h1>
-		<div v-if="quotePresent" class="quote mt-3">
-			<q>{{ quoteText }}</q>
-			<br />
-			<p>
-				<span id="quote-author">- {{ quoteAuthor }}</span>
+		<div
+			v-if="quotePresent"
+			aria-label="Inspirational quote"
+			class="quote quote-card mt-4"
+			role="note"
+		>
+			<p class="quote-label">Words to Work By</p>
+			<blockquote class="quote-text">
+				<p>{{ quoteText }}</p>
+			</blockquote>
+			<p class="quote-meta">
+				<span aria-hidden="true" class="quote-divider" />
+				<cite id="quote-author" class="quote-author">{{ quoteAuthor }}</cite>
 			</p>
 		</div>
 
@@ -89,26 +97,85 @@ onMounted(updateQuote);
 </template>
 
 <style scoped>
-span {
-	font-family: cursive;
-	margin-left: 30px;
-}
-
-.quote {
+.quote-card {
 	text-align: left;
-	width: 50%;
+	width: min(100%, 42rem);
 	margin: 0 auto;
-	background-color: whitesmoke;
+	padding: 1.5rem 1.75rem;
+	border: 1px solid rgba(120, 159, 190, 0.24);
+	border-radius: 24px;
+	background:
+		linear-gradient(145deg, rgba(255, 255, 255, 0.96), rgba(227, 242, 253, 0.92));
+	box-shadow: 0 18px 36px rgba(41, 72, 104, 0.12);
 }
 
-q,
-#quote-author {
+.quote-label {
+	margin: 0 0 0.85rem;
+	color: #557a98;
+	font-size: 0.78rem;
+	font-family: Verdana, Calibri, Candara, sans-serif;
+	font-weight: 700;
+	letter-spacing: 0.22em;
+	text-transform: uppercase;
+}
+
+.quote-text {
+	position: relative;
+	margin: 0;
+	padding-left: 1.35rem;
+}
+
+.quote-text::before {
+	content: "“";
+	position: absolute;
+	top: -0.45rem;
+	left: 0;
+	color: #8db0c8;
+	font-size: 3rem;
+	line-height: 1;
+	font-family: Georgia, "Times New Roman", serif;
+}
+
+.quote-text p {
+	margin: 0;
+	color: #243447;
+	font-size: 1.35rem;
+	line-height: 1.7;
+	text-align: left;
+	font-family: "Palatino Linotype", Georgia, serif;
 	font-style: italic;
 }
 
+.quote-meta {
+	display: flex;
+	align-items: center;
+	gap: 0.8rem;
+	margin: 1rem 0 0;
+	color: #546474;
+}
+
+.quote-divider {
+	display: inline-block;
+	width: 2.5rem;
+	height: 1px;
+	background-color: #7ca2bf;
+}
+
+#quote-author {
+	margin: 0;
+	font-style: normal;
+	font-size: 0.95rem;
+	font-weight: 600;
+}
+
 @media only screen and (max-width: 960px) {
-	.quote {
-		width: 80%;
+	.quote-card {
+		padding: 1.2rem 1.1rem;
+		border-radius: 20px;
+	}
+
+	.quote-text p {
+		font-size: 1.12rem;
 	}
 }
 </style>
