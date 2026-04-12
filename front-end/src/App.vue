eb8f94dfae5f7559cf64a357430fe7fc03a6b432
Jacob Anderson <jacobdanderson@gmail.com>
2026-04-12 16:18:05 -0400

    Add local branded favicon set

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -38,104 +38,111 @@ const structuredData = computed(() => [
 	}
 ]);
 
-useHead(() => ({
-	title: "Classes",
-	meta: [
-		{
-			name: "description",
-			content: siteDescription
-		},
-		{
-			property: "og:title",
-			content: "Classes with Jacob"
-		},
-		{
-			property: "og:description",
-			content: siteDescription
-		},
-		{
-			property: "og:type",
-			content: "website"
-		},
-		{
-			property: "og:url",
-			content: canonicalUrl.value
-		},
-		{
-			name: "twitter:card",
-			content: "summary"
-		},
-		{
-			name: "twitter:title",
-			content: "Classes with Jacob"
-		},
-		{
-			name: "twitter:description",
-			content: siteDescription
-		},
-		{
-			name: "robots",
-			content: robotsContent.value
-		},
-		{
-			name: "theme-color",
-			content: isDark.value ? "#00aba9" : "#ffffff"
-		}
-	],
-	link: [
-		{
-			rel: "icon",
-			type: "image/x-icon",
-			href: "https://classes-jacobdanderson.s3.us-east-1.amazonaws.com/Favicons/favicon.ico"
-		},
-		{
-			rel: "icon",
-			type: "image/png",
-			sizes: "32x32",
-			href: "https://classes-jacobdanderson.s3.us-east-1.amazonaws.com/Favicons/favicon-32x32.png"
-		},
-		{
-			rel: "icon",
-			type: "image/png",
-			sizes: "16x16",
-			href: "https://classes-jacobdanderson.s3.us-east-1.amazonaws.com/Favicons/favicon-16x16.png"
-		},
-		{
-			rel: "apple-touch-icon",
-			sizes: "180x180",
-			href: "https://classes-jacobdanderson.s3.us-east-1.amazonaws.com/Favicons/apple-touch-icon.png"
-		},
-		{
-			rel: "dns-prefetch",
-			href: "//scheduler.classes.jacobdanderson.net"
-		},
-		{
-			rel: "preconnect",
-			href: SCHEDULER_ORIGIN
-		},
-		{
-			rel: "canonical",
-			href: canonicalUrl.value
-		}
-	],
-	script: [
-		...(import.meta.env.PROD
-			? [
-					{
-						defer: true,
-						src: "https://analytics.jacobdanderson.net/script.js",
-						"data-website-id":
-							"d9905a72-7109-4f71-bfbb-d5e0dcef964e"
-					}
-				]
-			: []),
-		...structuredData.value.map((entry, index) => ({
-			innerHTML: JSON.stringify(entry),
-			key: `ld-json-${index}`,
-			type: "application/ld+json"
-		}))
-	]
-}) as any);
+useHead(
+	() =>
+		({
+			title: "Classes",
+			meta: [
+				{
+					name: "description",
+					content: siteDescription
+				},
+				{
+					property: "og:title",
+					content: "Classes with Jacob"
+				},
+				{
+					property: "og:description",
+					content: siteDescription
+				},
+				{
+					property: "og:type",
+					content: "website"
+				},
+				{
+					property: "og:url",
+					content: canonicalUrl.value
+				},
+				{
+					name: "twitter:card",
+					content: "summary"
+				},
+				{
+					name: "twitter:title",
+					content: "Classes with Jacob"
+				},
+				{
+					name: "twitter:description",
+					content: siteDescription
+				},
+				{
+					name: "robots",
+					content: robotsContent.value
+				},
+				{
+					name: "theme-color",
+					content: isDark.value ? "#13264c" : "#3158e8"
+				}
+			],
+			link: [
+				{
+					rel: "icon",
+					type: "image/svg+xml",
+					href: "/favicon.svg"
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png"
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png"
+				},
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png"
+				},
+				{
+					rel: "manifest",
+					href: "/site.webmanifest"
+				},
+				{
+					rel: "dns-prefetch",
+					href: "//scheduler.classes.jacobdanderson.net"
+				},
+				{
+					rel: "preconnect",
+					href: SCHEDULER_ORIGIN
+				},
+				{
+					rel: "canonical",
+					href: canonicalUrl.value
+				}
+			],
+			script: [
+				...(import.meta.env.PROD
+					? [
+							{
+								defer: true,
+								src: "https://analytics.jacobdanderson.net/script.js",
+								"data-website-id":
+									"d9905a72-7109-4f71-bfbb-d5e0dcef964e"
+							}
+						]
+					: []),
+				...structuredData.value.map((entry, index) => ({
+					innerHTML: JSON.stringify(entry),
+					key: `ld-json-${index}`,
+					type: "application/ld+json"
+				}))
+			]
+		}) as any
+);
 </script>
 
 <template>
